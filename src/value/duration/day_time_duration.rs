116f4ee610afee1a3489eaf3bbc4ee1a9cf58a0f
Timothée Haudebourg <timothee.haudebourg@spruceid.com>
2024-07-19 13:10:08 +0200

    Implement (de)serialization methods for `Duration`, `DayTimeDuration` & `YearMonthDuration`. Fixes #4

--- src/value/duration/day_time_duration.rs
+++ src/value/duration/day_time_duration.rs
@@ -1,5 +1,10 @@
-use crate::{format_nanoseconds, Datatype, DurationDatatype, ParseXsd, XsdValue};
+use crate::{
+	format_nanoseconds,
+	lexical::{duration::InvalidDayTimeDuration, LexicalFormOf},
+	Datatype, DurationDatatype, ParseXsd, XsdValue,
+};
 use core::fmt;
+use std::str::FromStr;
 
 #[derive(Debug, Clone, Copy)]
 pub struct DayTimeDuration {
@@ -23,6 +28,10 @@ impl DayTimeDuration {
 			nano_seconds,
 		}
 	}
+
+	pub fn into_string(self) -> String {
+		self.to_string()
+	}
 }
 
 impl XsdValue for DayTimeDuration {
@@ -33,6 +42,16 @@ impl XsdValue for DayTimeDuration {
 
 impl ParseXsd for DayTimeDuration {
 	type LexicalForm = crate::lexical::DayTimeDuration;
+}
+
+impl FromStr for DayTimeDuration {
+	type Err = InvalidDayTimeDuration<String>;
+
+	fn from_str(s: &str) -> Result<Self, Self::Err> {
+		let lexical_value = crate::lexical::DayTimeDuration::new(s)
+			.map_err(|InvalidDayTimeDuration(s)| InvalidDayTimeDuration(s.to_owned()))?;
+		Ok(lexical_value.as_value())
+	}
 }
 
 impl fmt::Display for DayTimeDuration {
@@ -78,5 +97,42 @@ impl fmt::Display for DayTimeDuration {
 		}
 
 		Ok(())
+	}
+}
+
+#[cfg(feature = "serde")]
+impl serde::Serialize for DayTimeDuration {
+	fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+	where
+		S: serde::Serializer,
+	{
+		self.into_string().serialize(serializer)
+	}
+}
+
+#[cfg(feature = "serde")]
+impl<'de> serde::Deserialize<'de> for DayTimeDuration {
+	fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+	where
+		D: serde::Deserializer<'de>,
+	{
+		struct Visitor;
+
+		impl<'de> serde::de::Visitor<'de> for Visitor {
+			type Value = DayTimeDuration;
+
+			fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+				formatter.write_str("a http://www.w3.org/2001/XMLSchema#dayTimeDuration")
+			}
+
+			fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+			where
+				E: serde::de::Error,
+			{
+				v.parse().map_err(|e| E::custom(e))
+			}
+		}
+
+		deserializer.deserialize_str(Visitor)
 	}
 }
--- src/value/duration/year_month_duration.rs
+++ src/value/duration/year_month_duration.rs
@@ -1,5 +1,9 @@
-use crate::{Datatype, DurationDatatype, ParseXsd, XsdValue};
+use crate::{
+	lexical::{duration::InvalidYearMonthDuration, LexicalFormOf},
+	Datatype, DurationDatatype, ParseXsd, XsdValue,
+};
 use core::fmt;
+use std::str::FromStr;
 
 #[derive(Debug, Clone, Copy)]
 pub struct YearMonthDuration {
@@ -14,6 +18,10 @@ impl YearMonthDuration {
 			months,
 		}
 	}
+
+	pub fn into_string(self) -> String {
+		self.to_string()
+	}
 }
 
 impl XsdValue for YearMonthDuration {
@@ -24,6 +32,16 @@ impl XsdValue for YearMonthDuration {
 
 impl ParseXsd for YearMonthDuration {
 	type LexicalForm = crate::lexical::YearMonthDuration;
+}
+
+impl FromStr for YearMonthDuration {
+	type Err = InvalidYearMonthDuration<String>;
+
+	fn from_str(s: &str) -> Result<Self, Self::Err> {
+		let lexical_value = crate::lexical::YearMonthDuration::new(s)
+			.map_err(|InvalidYearMonthDuration(s)| InvalidYearMonthDuration(s.to_owned()))?;
+		Ok(lexical_value.as_value())
+	}
 }
 
 impl fmt::Display for YearMonthDuration {
@@ -46,5 +64,42 @@ impl fmt::Display for YearMonthDuration {
 		}
 
 		Ok(())
+	}
+}
+
+#[cfg(feature = "serde")]
+impl serde::Serialize for YearMonthDuration {
+	fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+	where
+		S: serde::Serializer,
+	{
+		self.into_string().serialize(serializer)
+	}
+}
+
+#[cfg(feature = "serde")]
+impl<'de> serde::Deserialize<'de> for YearMonthDuration {
+	fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+	where
+		D: serde::Deserializer<'de>,
+	{
+		struct Visitor;
+
+		impl<'de> serde::de::Visitor<'de> for Visitor {
+			type Value = YearMonthDuration;
+
+			fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+				formatter.write_str("a http://www.w3.org/2001/XMLSchema#yearMonthDuration")
+			}
+
+			fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+			where
+				E: serde::de::Error,
+			{
+				v.parse().map_err(|e| E::custom(e))
+			}
+		}
+
+		deserializer.deserialize_str(Visitor)
 	}
 }
